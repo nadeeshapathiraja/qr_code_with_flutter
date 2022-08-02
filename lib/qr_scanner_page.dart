b8ba777b9502147a503925d2008e6125ed6b8bd9
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2022-08-02 09:41:22 +0530

    Bar code reader done

--- lib/qr_scanner_page.dart
+++ lib/qr_scanner_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class QRScannerPage extends StatefulWidget {
   const QRScannerPage({Key? key}) : super(key: key);
@@ -8,21 +11,82 @@ class QRScannerPage extends StatefulWidget {
 }
 
 class _QRScannerPageState extends State<QRScannerPage> {
+  final QRKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+  Barcode? barCode;
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
+    final size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black45,
+        body: Stack(
+          alignment: Alignment.center,
           children: [
-            Stack(
-              alignment: Alignment.center,
-              children: const [
-                // BuildQQView(context),
-              ],
+            BuildQQView(context),
+            Positioned(
+              bottom: 10,
+              child: buildRewsult(),
             ),
+            // Positioned(
+            //   top: 10,
+            //   child: buildControllerButtons(),
+            // ),
           ],
         ),
       ),
     );
   }
+
+  Widget BuildQQView(BuildContext context) => QRView(
+        key: QRKey,
+        onQRViewCreated: onQRViewCreated,
+        overlay: QrScannerOverlayShape(
+          borderWidth: 10,
+          borderLength: 20,
+          borderRadius: 10,
+          cutOutSize: MediaQuery.of(context).size.width * 0.8,
+        ),
+      );
+
+  void onQRViewCreated(QRViewController qrViewController) {
+    setState(() {
+      controller = qrViewController;
+    });
+
+    controller?.scannedDataStream
+        .listen((barCode) => setState(() => this.barCode = barCode));
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
+
+  buildRewsult() => Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white24,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Text(
+          barCode != null ? "Result ${barCode!.code}" : "Scan a code",
+          overflow: TextOverflow.ellipsis,
+          maxLines: 3,
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      );
 }
